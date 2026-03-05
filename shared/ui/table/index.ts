6369a16ee95d44887e7d5b95ae894feed4c0b2df
Vladimir <vl.petrosyan94@gmail.com>
2026-03-05 19:07:52 +0400

    refactor: remove deprecated table UI components

--- shared/ui/table/index.ts
+++
@@ -1,7 +0,0 @@
-// shared/ui/table/index.ts
-// Public API for table components and utilities
-export {
-  TanstackTableHeader,
-  TanstackTableBody,
-  DataTablePagination,
-} from "./ui-table/table";
--- shared/ui/table/ui-table/components/index.ts
+++
@@ -1,3 +0,0 @@
-// shared/ui/table/components/components/index.ts
-// Public API for table sub-components
-export { DataTablePagination } from "./TablePagination";
